08953225f6e5e606c5dbf18802e55b0c16f72d44
gedorinku <gedorinku@yahoo.co.jp>
2017-06-14 18:01:53 +0900

    出力を改善。

--- src/main/kotlin/io/github/gedorinku/supercon2017_test/Main.kt
+++ src/main/kotlin/io/github/gedorinku/supercon2017_test/Main.kt
@@ -77,11 +77,11 @@ fun runTest(nRange: IntRange = 1..20, probability: Int = 5) {
                 totalMilliSecondsYes += runResult.nanoTime / (1000 * 1000)
             }
 
-            if (totalCount % 1000 == 0) {
+            if (totalCount % 250 == 0) {
                 val mAverage = if (totalYesCount == 0) {
                     "NaN"
                 } else {
-                    (totalM / totalYesCount).toString()
+                    (totalM / totalYesCount.toDouble()).toString()
                 }
                 val yesMillisAverage = if (totalYesCount == 0) {
                     "NaN"
